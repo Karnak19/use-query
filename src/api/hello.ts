8c95bc60cfefdd2a50291bbcbfdf69638453ae63
Basile Vernouillet <basile64.v@gmail.com>
2022-09-28 02:42:27 +0200

    add defaultContext

--- src/api/hello.ts
+++ src/api/hello.ts
@@ -1,10 +1,11 @@
-import { useQuery } from '@tanstack/react-query';
+import { defaultContext, useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 
 function useHelloQuery(extra) {
   return useQuery({
     queryFn: () => axios.get('/api/hello').then((r) => r.data),
     queryKey: ['hello'],
+    context: defaultContext
     ...extra,
   });
 }
